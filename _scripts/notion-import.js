ffdef50f5098d29244dff5945bf1b713f47ac4e6
underthelights <kyuhwan.shim@sogang.ac.kr>
2023-11-16 15:30:21 +0900

    [UPDATE] notionimportjs

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -4,23 +4,18 @@ const moment = require("moment");
 const path = require("path");
 const fs = require("fs");
 const axios = require("axios");
-// or
-// import {NotionToMarkdown} from "notion-to-md";
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
-// passing notion client to the option
 const n2m = new NotionToMarkdown({ notionClient: notion });
 
 (async () => {
-  // ensure directory exists
   const root = "_posts";
   fs.mkdirSync(root, { recursive: true });
 
   const databaseId = process.env.DATABASE_ID;
-  // TODO has_more
   const response = await notion.databases.query({
     database_id: databaseId,
     filter: {
@@ -30,22 +25,21 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
       },
     },
   });
+
   for (const r of response.results) {
-    // console.log(r)
     const id = r.id;
-    // date
     let date = moment(r.created_time).format("YYYY-MM-DD");
     let pdate = r.properties?.["날짜"]?.["date"]?.["start"];
     if (pdate) {
       date = moment(pdate).format("YYYY-MM-DD");
     }
-    // title
+
     let title = id;
     let ptitle = r.properties?.["게시물"]?.["title"];
     if (ptitle?.length > 0) {
       title = ptitle[0]?.["plain_text"];
     }
-    // tags
+
     let tags = [];
     let ptags = r.properties?.["태그"]?.["multi_select"];
     for (const t of ptags) {
@@ -54,7 +48,7 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
         tags.push(n);
       }
     }
-    // categories
+
     let cats = [];
     let pcats = r.properties?.["카테고리"]?.["multi_select"];
     for (const t of pcats) {
@@ -64,7 +58,6 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
       }
     }
 
-    // frontmatter
     let fmtags = "";
     let fmcats = "";
     if (tags.length > 0) {
@@ -87,43 +80,47 @@ date: ${date}
 title: "${title}"${fmtags}${fmcats}
 ---
 `;
+
     const mdblocks = await n2m.pageToMarkdown(id);
-    const md = n2m.toMarkdownString(mdblocks)["parent"];
+    const md = n2m.toMarkdownString(mdblocks)?.["parent"]; // Added nullish coalescing operator here
 
-    const ftitle = `${date}-${title.replaceAll(" ", "-")}.md`;
+    if (md) {
+      const ftitle = `${date}-${title.replaceAll(" ", "-")}.md`;
 
-    let index = 0;
-    let edited_md = md.replace(
-      /(!\[\]\()(.*?)(\))/g,
-      function (match, p1, p2, p3) {
-        const dirname = path.join("assets/img", ftitle);
-        if (!fs.existsSync(dirname)) {
-          fs.mkdirSync(dirname, { recursive: true });
-        }
-        const filename = path.join(dirname, `${index}.png`);
+      let index = 0;
+      let edited_md = md.replace(
+        /(!\[\]\()(.*?)(\))/g,
+        function (match, p1, p2, p3) {
+          const dirname = path.join("assets/img", ftitle);
+          if (!fs.existsSync(dirname)) {
+            fs.mkdirSync(dirname, { recursive: true });
+          }
+          const filename = path.join(dirname, `${index}.png`);
 
-        axios({
-          method: "get",
-          url: p2,
-          responseType: "stream",
-        })
-          .then(function (response) {
-            let file = fs.createWriteStream(`${filename}`);
-            response.data.pipe(file);
+          axios({
+            method: "get",
+            url: p2,
+            responseType: "stream",
           })
-          .catch(function (error) {
-            console.log(error);
-          });
+            .then(function (response) {
+              let file = fs.createWriteStream(`${filename}`);
+              response.data.pipe(file);
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
 
-        return `![${index++}]` + `(/${filename})`; // avoid regex
-      }
-    );
+          return `![${index++}]` + `(/${filename})`;
+        }
+      );
 
-    //writing to file
-    fs.writeFile(path.join(root, ftitle), fm + edited_md, (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
+      fs.writeFile(path.join(root, ftitle), fm + edited_md, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    } else {
+      console.error("Markdown content is undefined for page ID:", id);
+    }
   }
 })();
